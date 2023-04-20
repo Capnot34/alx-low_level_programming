ad0d4b563b84a6c28a6af5196a7ee18618aea1fa
Capnot34 <jenniferamachree2@gmail.com>
2023-04-20 12:59:59 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, msg, sizeof(msg) - 1);
-    return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
