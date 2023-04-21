eee081ef62ea772b1167d65f0eefbf3d1c0b55d0
Capnot34 <jenniferamachree2@gmail.com>
2023-04-21 13:07:02 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints comma seperated numbers
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char hex;
+
+	for (hex = 0; hex < 10; hex++)
+	{
+		putchar(hex + '0');
+		if (hex < 9);
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
