fcbb68bf6858293a5ca2fd52884cfbf8f238ed70
Capnot34 <jenniferamachree2@gmail.com>
2023-04-21 07:03:08 -0700

    hurray

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text according number
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char alphabet;
+	char alphabet1;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+	    putchar(alphabet);
+	}
+	for (alphabet1 = 'A'; alphabet1 <= 'Z'; alphabet1++)
+	{
+	    putchar(alphabet1);
+	}
+	putchar('\n');
+	return (0);
+}
