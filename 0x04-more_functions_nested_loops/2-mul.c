693ccb2142200f3b9299655314c6cad11752f3f2
Capnot34 <jenniferamachree2@gmail.com>
2023-04-27 12:16:50 -0700

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - that multiplies two intergers
+ * @a: first input value to check
+ * @b: Sec value to check
+ * Return: a * b.
+ */
+
+int mul(int a, int b) 
+{
+    return a * b;
+}
