f030f3a5a9e01a0e2f833b4c8cd2d97e9470f5c6
Capnot34 <jenniferamachree2@gmail.com>
2023-05-02 10:39:10 -0700

    done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,10 +11,10 @@
 
 void _puts(char *str)
 {
-	 while (*str != '\0')
-	 {
-		 putchar(*str);
-		 str++;
-	 }
-	 putchar('\n');
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
 }
