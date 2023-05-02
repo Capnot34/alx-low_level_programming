e5862ad090a0eab989982fb62b10c1f584680b40
Capnot34 <jenniferamachree2@gmail.com>
2023-05-02 11:20:56 -0700

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse order
+ * @s: String to reverse
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	for (int i = len - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
