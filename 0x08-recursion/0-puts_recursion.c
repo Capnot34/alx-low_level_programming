2d08a685f87a72b755f20e6fe4abec1c8404658e
Capnot34 <jenniferamachree2@gmail.com>
2023-05-10 05:26:55 -0700

    done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0') /* base case */
-
 	{
-		 _putchar('\n');
-		 return;
+		_putchar('\n');
+		return;
 	}
 	_putchar(*s);
-	 _puts_recursion(s + 1); /* recursive call */
+	_puts_recursion(s + 1); /* recursive call */
 }
