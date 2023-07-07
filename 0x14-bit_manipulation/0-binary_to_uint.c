9bacb6cccb6b0310127333c56052bc30e3a0ef36
Capnot34 <jenniferamachree2@gmail.com>
2023-07-07 13:25:17 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,17 +7,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	unsigned int i = 0;
-	if (b == NULL)
-		 return 0;
+	int i;
+	unsigned int dec_val = 0;
 
-	while (b[i] != '\0')
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return 0;
-		result = (result << 1) + (b[i] - '0');
-		 i++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return result;
+	return (dec_val);
 }
