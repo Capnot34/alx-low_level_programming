b154afc948993dbfc99b29f811147a4703525a27
Capnot34 <jenniferamachree2@gmail.com>
2023-07-07 16:50:34 -0700

    done

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,7 +5,7 @@
  * @n: The first number
  * @m: The second number
  *
- * Return: The number of bits to flip
+ * Return: noflips
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
