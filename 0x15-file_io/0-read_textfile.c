ae50ca3d4a22305b7a55f1b67e2e8e6d5611099f
Capnot34 <jenniferamachree2@gmail.com>
2023-07-10 13:21:12 -0700

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,49 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file to be read
+ * @letters: Number of letters to read and print
+ *
+ * Return: The actual number of letters read and printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i, y;
+	ssize_t bytes_read, bytes_written;
 	char *buf;
+
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
 	{
-		free(buf);
+		close(fd);
 		return (0);
 	}
-	buf[i] = '\0';
-	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
+
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
 	{
 		free(buf);
+		close(fd);
 		return (0);
 	}
+
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
 	free(buf);
-	return (y);
+	close(fd);
+	return (bytes_written);
 }
