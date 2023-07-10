101966eb626025e3c2c4f7f365e5dad7dcb14566
Capnot34 <jenniferamachree2@gmail.com>
2023-07-10 12:58:22 -0700

    odne

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_r, fd_w, bytes_read, bytes_written;
+	int fd_r, fd_w;
+	ssize_t bytes_read, bytes_written;
 	char buffer[BUFSIZ];
 
 	if (argc != 3)
@@ -19,37 +20,41 @@ int main(int argc, char *argv[])
 	}
 
 	fd_r = open(argv[1], O_RDONLY);
-	if (fd_r < 0)
+	if (fd_r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_r);
+		exit(99);
+	}
+
 	while ((bytes_read = read(fd_r, buffer, BUFSIZ)) > 0)
 	{
-		if (fd_w < 0 || (bytes_written = write(fd_w, buffer, bytes_read)) != bytes_read)
+		bytes_written = write(fd_w, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_r);
+			close(fd_w);
 			exit(99);
 		}
 	}
 
-	if (bytes_read < 0)
+	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
-	if (close(fd_r) < 0 || close(fd_w) < 0)
+	if (close(fd_r) == -1 || close(fd_w) == -1)
 	{
-		if (close(fd_r) < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
-
-		if (close(fd_w) < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
-
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
 		exit(100);
 	}
 
