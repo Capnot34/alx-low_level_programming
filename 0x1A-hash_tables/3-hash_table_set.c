4de26cec537ec8dd0aee848aed3e1fa29818846b
Capnot34 <jenniferamachree2@github.com>
2023-09-27 16:17:13 -0700

    done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,24 +1,19 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds or updates a key/value pair in the hash table.
- * @ht: The hash table.
+ * update_key_value_if_exists - Update the value at
+ * a specific key if it exists.
+ * @head: Pointer to the beginning of the list at the index.
  * @key: The key.
  * @value: The value.
  *
- * Return: 1 on success, 0 on failure.
+ * Return: 1 if value updated, 0 otherwise.
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+static int update_key_value_if_exists(hash_node_t **head,
+		const char *key, const char *value)
 {
-	unsigned long int idx;
-	hash_node_t *new_node, *current;
+	hash_node_t *current = *head;
 
-	if (!ht || !key || !value || strlen(key) == 0)
-		return (0);
-
-	idx = key_index((const unsigned char *)key, ht->size);
-
-	current = ht->array[idx];
 	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
@@ -31,16 +26,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
+	return (0);
+}
 
-	new_node = malloc(sizeof(hash_node_t));
+/**
+ * create_hash_node - Create a new hash node with given key and value.
+ * @key: The key.
+ * @value: The value.
+ *
+ * Return: Pointer to the new hash node.
+ */
+static hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
 	if (!new_node)
-		return (0);
+		return (NULL);
 
 	new_node->key = strdup(key);
 	if (!new_node->key)
 	{
 		free(new_node);
-		return (0);
+		return (NULL);
 	}
 
 	new_node->value = strdup(value);
@@ -48,8 +55,35 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		free(new_node->key);
 		free(new_node);
-		return (0);
+		return (NULL);
 	}
+	return (new_node);
+}
+
+/**
+ * hash_table_set - Adds or updates a key/value pair in the hash table.
+ * @ht: The hash table.
+ * @key: The key.
+ * @value: The value.
+ *
+ * Return: 1 on success, 0 on failure.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *new_node;
+
+	if (!ht || !key || !value || strlen(key) == 0)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	if (update_key_value_if_exists(&(ht->array[idx]), key, value))
+		return (1);
+
+	new_node = create_hash_node(key, value);
+	if (!new_node)
+		return (0);
 
 	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
